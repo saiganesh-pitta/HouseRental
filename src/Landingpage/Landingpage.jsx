86e61e956bbd854eae7a17d28e025fe03fa9a272
saiganesh-pitta <saiganeshpitta5@gmail.com>
2025-08-15 00:42:45 +0530

    Update Landingpage.jsx

--- src/Landingpage/Landingpage.jsx
+++ src/Landingpage/Landingpage.jsx
@@ -4,6 +4,7 @@ import { States } from "../Context API/store";
 import { useContext, useEffect, useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
 import { IoMdAdd } from "react-icons/io";
+import { Bounce, ToastContainer, toast } from 'react-toastify';
 
   const Landingpage=()=>{
 
@@ -13,50 +14,51 @@ import { IoMdAdd } from "react-icons/io";
               Log()
            }, [])
     
-  //          const sentences = [
-  //   "Welcome to Rent a House.",
-  //   "Find your perfect House.",
-  //   "Easy way of finding home."
-  //   "Find within your Locations."
-  //   "Get Directions of Houses."
-  // ];
+           const sentences = [
+    "Welcome to Rent a House.",
+    "Find your perfect House.",
+    "Easy way of finding home."
+    "Find within your Locations."
+    "Get Directions of Houses."
+  ];
 
-  // const [text, setText] = useState("");
-  // const [sentenceIndex, setSentenceIndex] = useState(0);
-  // const [charIndex, setCharIndex] = useState(0);
-  // const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [sentenceIndex, setSentenceIndex] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
 
-  // useEffect(() => {
-  //   const currentSentence = sentences[sentenceIndex];
-  //   let typingSpeed = isDeleting ? 50 : 100; // speed while deleting or typing
+  useEffect(() => {
+    const currentSentence = sentences[sentenceIndex];
+    let typingSpeed = isDeleting ? 50 : 100; // speed while deleting or typing
 
-  //   const timer = setTimeout(() => {
-  //     if (!isDeleting) {
-  //       // Typing
-  //       setText(currentSentence.substring(0, charIndex + 1));
-  //       setCharIndex(prev => prev + 1);
+    const timer = setTimeout(() => {
+      if (!isDeleting) {
+        // Typing
+        setText(currentSentence.substring(0, charIndex + 1));
+        setCharIndex(prev => prev + 1);
 
-  //       if (charIndex === currentSentence.length) {
-  //         // Pause before deleting
-  //         setTimeout(() => setIsDeleting(true), 1000);
-  //       }
-  //     } else {
-  //       // Deleting
-  //       setText(currentSentence.substring(0, charIndex - 1));
-  //       setCharIndex(prev => prev - 1);
+        if (charIndex === currentSentence.length) {
+          // Pause before deleting
+          setTimeout(() => setIsDeleting(true), 1000);
+        }
+      } else {
+        // Deleting
+        setText(currentSentence.substring(0, charIndex - 1));
+        setCharIndex(prev => prev - 1);
 
-  //       if (charIndex === 0) {
-  //         setIsDeleting(false);
-  //         setSentenceIndex((prev) => (prev + 1) % sentences.length);
-  //       }
-  //     }
-  //   }, typingSpeed);
+        if (charIndex === 0) {
+          setIsDeleting(false);
+          setSentenceIndex((prev) => (prev + 1) % sentences.length);
+        }
+      }
+    }, typingSpeed);
 
-  //   return () => clearTimeout(timer);
-  // }, [charIndex, isDeleting, sentenceIndex]);
+    return () => clearTimeout(timer);
+  }, [charIndex, isDeleting, sentenceIndex]);
 
 
     return (<>
+         <ToastContainer/>
     <div className="landing-cont" >
         <div className="typing-cont" >
            <h2 className="typing">
