c299af57fb3ec7ef3a671293ea543dceb92f653b
saiganesh-pitta <saiganeshpitta5@gmail.com>
2025-08-15 16:45:29 +0530

    Update Filter.jsx

--- src/AllHomes/Filter.jsx
+++ src/AllHomes/Filter.jsx
@@ -16,9 +16,12 @@ import { States } from "../Context API/store.jsx";
     <div className="filter-bar" >
       <select name="location" className="filter-options" value={filters.area} onChange={handleChange}>
         <option value="">Select Area</option>
-        <option value="Hyderabad">Hyderabad</option>
-        <option value="Bangalore">Bangalore</option>
-        <option value="Chennai">Chennai</option>
+           <option value="Hyderabad">Hyderabad</option>
+            <option value="Warangal">Warangal</option>
+            <option value="Bangalore">Bangalore</option>
+            <option value="Pune">Pune</option>
+            <option value="Chennai">Chennai</option>
+            <option value="Mumbai">Mumbai</option>
       </select>
 
       <select name="BHK" className="filter-options" value={filters.bhk} onChange={handleChange}>
@@ -26,7 +29,7 @@ import { States } from "../Context API/store.jsx";
         <option value="1">1 BHK</option>
         <option value="2">2 BHK</option>
         <option value="3">3 BHK</option>
-        <option value="4">4+ BHK</option>
+        <option value="4">4 BHK</option>
       </select>
     </div>
   );
