530d958a9f7dd1da6cbdca5ab10a3a6def1f1a15
saiganesh-pitta <saiganeshpitta5@gmail.com>
2025-08-15 15:53:17 +0530

    Update Settings.jsx

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -141,7 +141,7 @@ const Settings = () => {
       <footer className="settings-footer">
         <p>------- developed by pitta saiganesh -------</p>
       </footer>
-      <center><p className="note" >note: login with apple device there is a cookies-sessions issue </p></center>
+      <center><p className="note" >note: login with apple device there is a Login-sessions issue </p></center>
     </>
   );
 };
